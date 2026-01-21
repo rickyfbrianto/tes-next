ccc9def2480381fcb60b8ea68bd93cae740a34e4
rickyfbrianto <rickyfbrianto50@ymail.com>
2026-01-21 23:05:46 +0800

    Tambahan 1 kolom di header

--- components/header.tsx
+++ components/header.tsx
@@ -5,6 +5,7 @@ export default function Header() {
     return (
         <div className='flex items-center h-[6rem] bg-stone-300 gap-5 p-5'>
             <Link href={'/'}>Beranda</Link>
+            <Link href={'/product'}>Product</Link>
             <Link href={'/berita'}>Berita</Link>
             <Link href={'/about'}>About</Link>
         </div >
