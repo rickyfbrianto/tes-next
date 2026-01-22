ae8d6dfd1b93fe79c72c799dc0c062c74b261065
rickyfbrianto <rickyfbrianto50@ymail.com>
2026-01-22 19:50:00 +0800

    Tambahan Card di product

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -5,6 +5,13 @@ export default function page() {
         <div>
             <h1>ini halaman product</h1>
             <span>hello!</span>
+            <div className="flex flex-col gap-2 p-4 border rounded-lg">
+                <span className='text-[1.2rem] font-bold italic'>Hai user</span>
+                <div className="flex gap-4">
+                    <span>Deskripsi</span>
+                    <span className='indent-[2rem] first-letter:italic first-letter:text-[2rem] '>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga error qui provident minus praesentium autem ad neque saepe, quas asperiores modi numquam libero consectetur mollitia facere delectus itaque! Dolorem, ratione.</span>
+                </div>
+            </div>
         </div>
     )
 }
