ab690ca76e8e12b67bb00cad22a95aaa502ccdb8
Tjoa Ricky Febrianto <ricky@sagatrade.co.id>
2026-01-23 18:23:42 +0800

    Tambahan Mni Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,7 @@ export default function Sidebar() {
     return (
         <div className='flex flex-col gap-4 min-w-[15rem] bg-zinc-100 p-4'>
             <h1>Sidebar</h1>
+            <span>Ini adalah mini sidebar</span>
         </div>
     )
 }
